557a58afc3df664b859c47090bf8bef28793127e
kungmin19 <natthawutkingsanthia@gmail.com>
2026-03-24 05:20:16 +0700

    Configure app to run on specified port

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os 
 from epl import create_app, db
+
 app = create_app()
 
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
